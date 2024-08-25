6fe5f99cb19628160bdbb0944007449e67e53628
Shivam25092001 <ss056989@gmail.com>
2024-08-25 21:01:24 +0530

    chore: demo changes

--- packages/react-native-lib-1/example/src/App.tsx
+++ packages/react-native-lib-1/example/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Lib 1 Result: {result}</Text>
     </View>
   );
 }
