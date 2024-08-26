b16e0fade022f9003d282b6c5e7d3b691a76d163
Shivam25092001 <ss056989@gmail.com>
2024-08-26 17:27:27 +0530

    chore: some random change

--- packages/react-native-lib-1/example/src/App.tsx
+++ packages/react-native-lib-1/example/src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Some Text</Text>
+      <Text>Some Changed Text</Text>
       <Text>Result from lib-1: {result}</Text>
     </View>
   );
